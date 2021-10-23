a4c1d7605c91c705ad618f09d1158db3d43460aa
Vikulov Andrey <vikulov.prog@gmail.com>
2021-10-24 00:16:04 +0400

    finish

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,4 +1,20 @@
-'use strict'
+const swiper = new Swiper('.testimonial__swiper', {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+        el: '.testimonial__swiper--pagination',
+        type: 'bullets',
+        clickable: true,
+        bulletActiveClass: "testimonial__swiper--bullet-active",
+        bulletClass: "testimonial__swiper--bullet"
+
+
+    },
+    centeredSlides: true,
+    createElements: true,
+});
+
+
 window.addEventListener('load', () => {
 
 });
